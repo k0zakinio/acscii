48b543fc6df9ee261e87332c19e90b4e029ebd1f
Andrew Dowell <adowellphoto@gmail.com>
2016-03-27 14:19:41 +0100

    attempting to split into Cell crashes, commiting before something messes up

--- Canvas.java
+++ Canvas.java
@@ -1,20 +1,38 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.HashSet;
+
 public class Canvas {
   int height, width;
   String[][] canvas;
-  public void createCanvas(int h, int w) {
+  Cell[] cells;
+
+  public void createCanvas(int w, int h) {
     this.height = h;
     this.width = w;
-    this.generateCanvas();
+    generateCanvas();
   }
 
-  public void generateCanvas() {
+  private void generateCanvas() {
     this.canvas = new String[this.height][this.width];
     for(int row = 0; row < this.height; row++) {
       for(int col = 0; col < this.width; col++) {
 	this.canvas[row][col] = " ";
       }
     }
+  }
+
+  public void generateCellList() {
+    int size = this.width * this.height;
+
+    Cell[] cellList = new Cell[size];
+
+    for(int index = 0; index < cellList.length; index++) {
+      int y = (int) (Math.floor(index/this.height));
+      int x = (int) (Math.floor(index - (y * this.height)));
+      cellList[index] = new Cell(x, y);
+    }
+    this.cells = cellList;
   }
 
   public void renderCanvas() {
@@ -63,6 +81,43 @@ public class Canvas {
     drawLine(x1, y2, x2, y2);
   }
 
+  //public void bucketFill(int x, int y, char filler, HashSet<int[]> hset) {
+  //  // CHECK IF A PREVIOUS ARRAY HAS BEEN SUPPLIED, IF NOT MAKE A NEW ONE
+  //  HashSet<int[]> prevHash;
+  //  if(hset != null) {
+  //    prevHash = hset;
+  //  } else {
+  //    prevHash = new HashSet<int[]>();
+  //  }
+  //  // CHECK THE SURROUNDING CELLS FOR POSSIBLE FILLS
+  //  int[][] neighbours = new int[][] {
+  //      			      	{0,-1},// N  
+  //      			      	{1,-1},// NE
+  //      			      	{1,0}, // E
+  //      				{1,1}, // SE
+  //      			      	{0,1}, // S
+  //      			      	{-1,1},// SW
+  //      			      	{-1,0},// W
+  //      			      	{-1,-1}// NW
+  //      			     };
+  //  for(int[] n: neighbours) {
+  //    int[] toFill = new int[] {x + n[0], y + n[1]};
+  //    if(canFill(toFill[0], toFill[1]) && !prevHash.contains(toFill)) {
+  //      prevHash.add(toFill);
+  //      //this.bucketFill(toFill[0], toFill[1], "o", prevHash);
+  //      System.out.println(prevHash);
+  //      bucketFill(toFill[0], toFill[1], 'o', prevHash);
+  //    }
+  //  }
+  //}
+
+  private boolean canFill(int x, int y) {
+    if(x < this.width && x >= 0 && y < this.height && y >= 0 && !this.canvas[y][x].equals("x")) {
+      return true;
+    }
+    return false;
+  }
+
   public void parseInput(String input) {
     String[] splitInput = input.split(" ");
     String command = splitInput[0];
@@ -100,11 +155,16 @@ public class Canvas {
   }
 
   public static void main(String[] args) {
-    Scanner keyboard = new Scanner(System.in);
+    //Scanner keyboard = new Scanner(System.in);
+    //Canvas instance = new Canvas();
+    //while(true) {
+    //  System.out.print("Enter command: ");
+    //  instance.parseInput(keyboard.nextLine()); 
+    //}
+    
     Canvas instance = new Canvas();
-    while(true) {
-      System.out.print("Enter command: ");
-      instance.parseInput(keyboard.nextLine()); 
-    }
+    instance.createCanvas(15, 5);
+    instance.generateCellList();
+    System.out.println(instance.cells[1]);
   }
-
+}
---
+++ Cell.java
@@ -0,0 +1,15 @@
+public class Cell {
+  int x, y;
+  char val;
+  public Cell(int x, int y) {
+    this.val = 'x';
+    this.x = x;
+    this.y = y;
+  }
+
+  public void point() {
+    this.val = 'x';
+  }
+
+
+}
